a9b4a19a108564da96f8703d99cab6bf1646615b
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2018-06-20 11:54:13 +0200

    fixes

--- test/contracts/lib/safe_math_extensions.js
+++ test/contracts/lib/safe_math_extensions.js
@@ -12,7 +12,7 @@ import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import web3jsChai from '../../helpers/events';
 import {createWeb3, deployContract} from '../../../src/web3_tools';
-import SafeMath2Json from '../../../build/contracts/SafeMathExtensions.json';
+import SafeMathExtensions from '../../../build/contracts/SafeMathExtensions.json';
 
 
 chai.use(web3jsChai());
@@ -24,25 +24,25 @@ const {expect} = chai;
 
 describe('SafeMathExtensions', () => {
   let web3;
-  let safeMath2;
+  let safeMathExtensions;
 
   beforeEach(async () => {
     web3 = await createWeb3();
-    safeMath2 = await deployContract(web3, SafeMath2Json);    
+    safeMathExtensions = await deployContract(web3, SafeMathExtensions);    
   });
 
   it('pow2', async () => {
-    expect(await safeMath2.methods.safePow2(0).call()).to.equal('1');
-    expect(await safeMath2.methods.safePow2(1).call()).to.equal('2');
-    expect(await safeMath2.methods.safePow2(2).call()).to.equal('4');
-    expect(await safeMath2.methods.safePow2(3).call()).to.equal('8');
-    expect(await safeMath2.methods.safePow2(4).call()).to.equal('16');
-    expect(await safeMath2.methods.safePow2(16).call()).to.equal('65536');
-    expect(await safeMath2.methods.safePow2(24).call()).to.equal('16777216');
-    expect(await safeMath2.methods.safePow2(100).call()).to.equal('1267650600228229401496703205376');
-    expect(await safeMath2.methods.safePow2(255).call()).to.equal('57896044618658097711785492504343953926634992332820282019728792003956564819968');
-    await expect(safeMath2.methods.safePow2(256).call()).to.be.eventually.rejected;
-    await expect(safeMath2.methods.safePow2(300).call()).to.be.eventually.rejected;
-    await expect(safeMath2.methods.safePow2(1000).call()).to.be.eventually.rejected;
+    expect(await safeMathExtensions.methods.safePow2(0).call()).to.equal('1');
+    expect(await safeMathExtensions.methods.safePow2(1).call()).to.equal('2');
+    expect(await safeMathExtensions.methods.safePow2(2).call()).to.equal('4');
+    expect(await safeMathExtensions.methods.safePow2(3).call()).to.equal('8');
+    expect(await safeMathExtensions.methods.safePow2(4).call()).to.equal('16');
+    expect(await safeMathExtensions.methods.safePow2(16).call()).to.equal('65536');
+    expect(await safeMathExtensions.methods.safePow2(24).call()).to.equal('16777216');
+    expect(await safeMathExtensions.methods.safePow2(100).call()).to.equal('1267650600228229401496703205376');
+    expect(await safeMathExtensions.methods.safePow2(255).call()).to.equal('57896044618658097711785492504343953926634992332820282019728792003956564819968');
+    await expect(safeMathExtensions.methods.safePow2(256).call()).to.be.eventually.rejected;
+    await expect(safeMathExtensions.methods.safePow2(300).call()).to.be.eventually.rejected;
+    await expect(safeMathExtensions.methods.safePow2(1000).call()).to.be.eventually.rejected;
   });
 });
