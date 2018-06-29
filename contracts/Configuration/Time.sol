55b81d3917486bbf7c49b2d58baa40e903efa54a
Antoni Kedracki <antoni@ethworks.io>
2018-06-29 15:54:36 +0200

    time contract implemented and tested;

--- contracts/Configuration/Time.sol
+++ contracts/Configuration/Time.sol
@@ -9,7 +9,28 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 
 pragma solidity ^0.4.23;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Time {
-    constructor () public { }
+    using SafeMath for uint;
+
+    function currentTimestamp() public view returns(uint) {
+        return now;
+    }
+
+    function currentPayoutPeriod() public view returns (uint64) {
+        return payoutPeriodForTimestamp(currentTimestamp());
+    }
+
+    function payoutPeriodForTimestamp(uint timestamp) public pure returns(uint64) {
+        return (uint64)(timestamp.div(28 days));
+    }
+
+    function timestampForBeginOfPayoutPeriod(uint64 period) public pure returns(uint) {
+        return (uint)(period).mul(28 days);
+    }
+
+    function secondsSinceBeginOfPayoutPeriod(uint timestamp) public pure returns(uint) {
+        return timestamp.sub(timestampForBeginOfPayoutPeriod(payoutPeriodForTimestamp(timestamp)));
+    }
 }
