de00bfaacf7723d286c25fa64d4a294f965c3823
Bartłomiej Rutkowski <bartek@ethworks.io>
2018-07-18 17:41:53 +0200

    RolesStore introduced

---
+++ contracts/Storage/RolesStore.sol
@@ -0,0 +1,48 @@
+/*
+Copyright: Ambrosus Technologies GmbH
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+pragma solidity ^0.4.23;
+
+import "../Boilerplate/Head.sol";
+import "../Configuration/Config.sol";
+import "./KycWhitelist.sol";
+
+contract RolesStore is Base {
+
+    struct NodeInfo {
+        Config.NodeType role;
+        string url;
+    }
+
+    mapping(address => NodeInfo) public nodes;
+
+    constructor(Head _head) public Base(_head) { }
+
+    function setRole(address node, Config.NodeType role) public onlyContextInternalCalls {
+        if (role != Config.NodeType.NONE) {
+            KycWhitelist kycWhitelist = context().kycWhitelist();
+            require(kycWhitelist.hasRoleAssigned(node, role));
+        }
+
+        nodes[node].role = role;
+    }
+
+    function setUrl(address node, string url) public onlyContextInternalCalls {
+        require(getRole(node) == Config.NodeType.ATLAS || getRole(node) == Config.NodeType.HERMES);
+        nodes[node].url = url;
+    }
+
+    function getRole(address node) public view returns(Config.NodeType) {
+        return nodes[node].role;
+    }
+
+    function getUrl(address node) public view returns(string) {
+        return nodes[node].url;
+    }
+}
