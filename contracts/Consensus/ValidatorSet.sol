7cce0b9074115f865cdcb640e79f6a9264d6eb92
Antoni Kedracki <antoni@ethworks.io>
2018-08-28 13:23:20 +0200

    style improvements and making the linter happy;

--- contracts/Consensus/ValidatorSet.sol
+++ contracts/Consensus/ValidatorSet.sol
@@ -9,6 +9,7 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 
 pragma solidity ^0.4.22;
 
+
 // From: https://wiki.parity.io/Validator-Set.html
 contract ValidatorSetBase {
     event InitiateChange(bytes32 indexed parentHash, address[] newSet);
@@ -16,6 +17,7 @@ contract ValidatorSetBase {
     function getValidators() public view returns (address[]); 
     function finalizeChange() public;
 }
+
 
 /**
 @title Implementation of Parities ValidatorSet contract with:
@@ -25,41 +27,11 @@ contract ValidatorSetBase {
 contract ValidatorSet is ValidatorSetBase {
     address public owner;
     // the SUPER_USER address as defined by EIP96. During normal operation should be 2**160 - 2
-    address private super_user;
+    address private superUser;
     address[] public validators;
     address[] public pendingValidators;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-    function checkInArray(address _subject, address[] _array) private pure returns(bool) {
-        for (uint i = 0; i < _array.length; ++i) {
-            if (_array[i] == _subject) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    modifier inArray(address _subject, address[] _array) {
-        require(checkInArray(_subject, _array));
-        _;
-    }
-
-    modifier notInArray(address _subject, address[] _array) {
-        require(!checkInArray(_subject, _array));
-        _;
-    }
 
     /**
     @notice Constructor
@@ -67,11 +39,32 @@ contract ValidatorSet is ValidatorSetBase {
     @param _initialValidators the initial set of validator addresses.
     @param _super_user SUPER_USER address value injectable for test purpouses. Under normal operation it should be set to 2^160-2 as defined in EIP96 
     */
-    constructor(address _owner, address[] _initialValidators, address _super_user) public {
+    constructor(address _owner, address[] _initialValidators, address _superUser) public {
         owner = _owner;
-        super_user = _super_user;
+        superUser = _superUser;
         validators = _initialValidators;
         pendingValidators = _initialValidators;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "sender must be owner");
+        _;
+    }
+
+    modifier inArray(address _subject, address[] _array, string _message) {
+        require(checkInArray(_subject, _array), _message);
+        _;
+    }
+
+    modifier notInArray(address _subject, address[] _array, string _message) {
+        require(!checkInArray(_subject, _array), _message);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner must not be 0x0");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
     }
 
     function getValidators() public view returns (address[]) {
@@ -82,12 +75,12 @@ contract ValidatorSet is ValidatorSetBase {
         return pendingValidators;
     }
 
-    function addValidator(address _validator) public onlyOwner notInArray(_validator, pendingValidators) {
+    function addValidator(address _validator) public onlyOwner notInArray(_validator, pendingValidators, "Provided address is already a validator") {
         pendingValidators.push(_validator);    
         emitChangeEvent();
     }
 
-    function removeValidator(address _validator) public onlyOwner inArray(_validator, pendingValidators) {
+    function removeValidator(address _validator) public onlyOwner inArray(_validator, pendingValidators, "Provided address is not a validator") {
         for (uint i = 0; i < pendingValidators.length; ++i) {
             if (pendingValidators[i] == _validator) {
                 pendingValidators[i] = pendingValidators[pendingValidators.length - 1];
@@ -99,12 +92,21 @@ contract ValidatorSet is ValidatorSetBase {
     }
 
     function finalizeChange() public {
-        require(msg.sender == super_user);
+        require(msg.sender == superUser, "Must be called by super user");
         validators = pendingValidators;
     }
 
     function emitChangeEvent() private {
         /* solium-disable-next-line security/no-block-members */
         emit InitiateChange(blockhash(block.number - 1), pendingValidators);
+    }
+
+    function checkInArray(address _subject, address[] _array) private pure returns(bool) {
+        for (uint i = 0; i < _array.length; ++i) {
+            if (_array[i] == _subject) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- test/contracts/lib/safe_math_extensions.js
+++ test/contracts/lib/safe_math_extensions.js
@@ -11,7 +11,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import {createWeb3, deployContract} from '../../../src/web3_tools';
-import SafeMathExtensionsAdapter from '../../../build/contracts/SafeMathExtensionAdapter.json';
+import SafeMathExtensionsAdapter from '../../../build/contracts/SafeMathExtensionsAdapter.json';
 import BN from 'bn.js';
 import {ONE} from '../../helpers/consts';
 
