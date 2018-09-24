6c39a906a9cd276dce8f2cc0498fb16d7089c09f
Antoni Kedracki <antoni@ethworks.io>
2018-09-24 13:12:16 +0200

    Atlas store now keeps track of the number of staking atlas nodes; atlas skate store test refactored;

--- contracts/Storage/AtlasStakeStore.sol
+++ contracts/Storage/AtlasStakeStore.sol
@@ -10,12 +10,15 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 pragma solidity ^0.4.23;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../Lib/SafeMathExtensions.sol";
 import "../Boilerplate/Head.sol";
 
 
 contract AtlasStakeStore is Base {
 
     using SafeMath for uint;
+    using SafeMath for uint32;
+    using SafeMathExtensions for uint;
 
     struct Stake {
         uint initialAmount;
@@ -28,11 +31,16 @@ contract AtlasStakeStore is Base {
     }
 
     mapping (address => Stake) stakes;
+    uint32 numberOfStakers;
 
     constructor(Head _head) public Base(_head) {} 
 
     function isStaking(address node) public view returns (bool) {
-        return stakes[node].amount > 0;
+        return stakes[node].initialAmount > 0;
+    }
+
+    function getNumberOfStakers() public view returns (uint32) {
+        return numberOfStakers;
     }
 
     function canStore(address node) public view returns (bool) {
@@ -55,22 +63,15 @@ contract AtlasStakeStore is Base {
         return stakes[node].storageUsed > 0;
     }
 
-    function getLastChallengeResolvedSequenceNumber(address node) public view returns (uint) {
-        return stakes[node].lastChallengeResolvedSequenceNumber;
-    }
-
     function getBasicStake(address node) public view returns (uint) {
         return stakes[node].initialAmount;
     }
 
     function depositStake(address _who, uint _storageLimit) public payable onlyContextInternalCalls {
         require(!isStaking(_who));
+        
         stakes[_who] = Stake(msg.value, msg.value, _storageLimit, 0, 0, 0, 0);
-    }
-
-    function updateLastChallengeResolvedSequenceNumber(address node, uint updatedLastChallengeResolvedSequenceNumber) public onlyContextInternalCalls {
-        require(isStaking(node));
-        stakes[node].lastChallengeResolvedSequenceNumber = updatedLastChallengeResolvedSequenceNumber;
+        numberOfStakers = numberOfStakers.add(1).castTo32();
     }
 
     function releaseStake(address node, address refundAddress) public onlyContextInternalCalls returns(uint) {
@@ -78,24 +79,16 @@ contract AtlasStakeStore is Base {
         require(!isShelteringAny(node));
         uint amount = stakes[node].amount;
         delete stakes[node];
+        numberOfStakers = numberOfStakers.sub(1).castTo32();
         refundAddress.transfer(amount);
         return amount;
     }
 
-    function decrementStorageUsed(address node) public onlyContextInternalCalls {
-        require(isShelteringAny(node));
-        stakes[node].storageUsed = stakes[node].storageUsed.sub(1);
-    }
-
-    function incrementStorageUsed(address node) public onlyContextInternalCalls {
-        require(canStore(node));
-        stakes[node].storageUsed = stakes[node].storageUsed.add(1);
-    }
-
     function slash(address shelterer, address refundAddress, uint penaltyAmount)
-    public onlyContextInternalCalls returns(uint)
+        public onlyContextInternalCalls returns(uint)
     {
         require(isStaking(shelterer));
+        require(stakes[shelterer].amount > 0);
 
         uint slashedAmount;
         if (penaltyAmount > stakes[shelterer].amount) {
@@ -108,6 +101,16 @@ contract AtlasStakeStore is Base {
         return slashedAmount;
     }
 
+    function decrementStorageUsed(address node) public onlyContextInternalCalls {
+        require(isShelteringAny(node));
+        stakes[node].storageUsed = stakes[node].storageUsed.sub(1);
+    }
+
+    function incrementStorageUsed(address node) public onlyContextInternalCalls {
+        require(canStore(node));
+        stakes[node].storageUsed = stakes[node].storageUsed.add(1);
+    }
+
     function getPenaltiesHistory(address node) public view returns (uint penaltiesCount, uint64 lastPenaltyTime) {
         penaltiesCount = stakes[node].penaltiesCount;
         lastPenaltyTime = stakes[node].lastPenaltyTime;
@@ -116,5 +119,14 @@ contract AtlasStakeStore is Base {
     function setPenaltyHistory(address shelterer, uint penaltiesCount, uint64 currentTimestamp) public onlyContextInternalCalls {
         stakes[shelterer].penaltiesCount = penaltiesCount;
         stakes[shelterer].lastPenaltyTime = currentTimestamp;
+    }
+
+    function getLastChallengeResolvedSequenceNumber(address node) public view returns (uint) {
+        return stakes[node].lastChallengeResolvedSequenceNumber;
+    }
+
+    function updateLastChallengeResolvedSequenceNumber(address node, uint updatedLastChallengeResolvedSequenceNumber) public onlyContextInternalCalls {
+        require(isStaking(node));
+        stakes[node].lastChallengeResolvedSequenceNumber = updatedLastChallengeResolvedSequenceNumber;
     }
 }
