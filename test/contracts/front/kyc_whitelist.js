883f4840dac2c95150228c5daa8513a9ab284dd3
Antoni Kedracki <antoni@ethworks.io>
2018-10-23 17:56:08 +0200

    fixed KYC whitelist tests

--- test/contracts/front/kyc_whitelist.js
+++ test/contracts/front/kyc_whitelist.js
@@ -22,24 +22,25 @@ const {expect} = chai;
 
 describe('KYC Whitelist Contract', () => {
   let web3;
-  let from;
-  let other;
-  let totalStranger;
+  let deployer;
+  let candidate;
+  let notOwner;
   let kycWhitelist;
   let snapshotId;
 
   const isWhitelisted = async (address) => kycWhitelist.methods.isWhitelisted(address).call();
   const hasRoleAssigned = async (address, role) => kycWhitelist.methods.hasRoleAssigned(address, role).call();
   const getRequiredDeposit = async (address) => kycWhitelist.methods.getRequiredDeposit(address).call();
-  const addToWhitelist = async (address, role, deposit, sender = from) => kycWhitelist.methods.add(address, role, deposit).send({from: sender});
-  const removeFromWhitelist = async (address, sender = from) => kycWhitelist.methods.remove(address).send({from: sender});
+  const addToWhitelist = async (address, role, deposit, sender = deployer) => kycWhitelist.methods.add(address, role, deposit).send({from: sender});
+  const removeFromWhitelist = async (address, sender = deployer) => kycWhitelist.methods.remove(address).send({from: sender});
   const getRoleAssigned = async (address) => kycWhitelist.methods.getRoleAssigned(address).call();
 
   before(async () => {
     web3 = await createWeb3();
-    [from, other, totalStranger] = await web3.eth.getAccounts();
+    [deployer, candidate, notOwner] = await web3.eth.getAccounts();
     ({kycWhitelist} = await deploy({
       web3,
+      sender: deployer,
       contracts: {
         kycWhitelistStore: true,
         kycWhitelist: true,
@@ -57,85 +58,85 @@ describe('KYC Whitelist Contract', () => {
   });
 
   it(`adds and removes from whitelist, checks if address is whitelisted`, async () => {
-    expect(await isWhitelisted(other)).to.equal(false);
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    expect(await isWhitelisted(other)).to.equal(true);
-    await removeFromWhitelist(other);
-    expect(await isWhitelisted(other)).to.equal(false);
+    expect(await isWhitelisted(candidate)).to.equal(false);
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    expect(await isWhitelisted(candidate)).to.equal(true);
+    await removeFromWhitelist(candidate);
+    expect(await isWhitelisted(candidate)).to.equal(false);
   });
 
   it('works for ATLAS1_STAKE', async () => {
-    await expect(addToWhitelist(other, ATLAS, ATLAS1_STAKE)).to.be.fulfilled;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS1_STAKE)).to.be.fulfilled;
   });
 
   it('works for ATLAS2_STAKE', async () => {
-    await expect(addToWhitelist(other, ATLAS, ATLAS2_STAKE)).to.be.fulfilled;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS2_STAKE)).to.be.fulfilled;
   });
 
   it('works for ATLAS3_STAKE', async () => {
-    await expect(addToWhitelist(other, ATLAS, ATLAS3_STAKE)).to.be.fulfilled;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS3_STAKE)).to.be.fulfilled;
   });
 
   it('has no role permitted before whitelisting', async () => {
-    expect(await getRoleAssigned(other)).to.equal('0');
-    expect(await hasRoleAssigned(other, ATLAS)).to.be.false;
-    expect(await hasRoleAssigned(other, HERMES)).to.be.false;
-    expect(await hasRoleAssigned(other, APOLLO)).to.be.false;
+    expect(await getRoleAssigned(candidate)).to.equal('0');
+    expect(await hasRoleAssigned(candidate, ATLAS)).to.be.false;
+    expect(await hasRoleAssigned(candidate, HERMES)).to.be.false;
+    expect(await hasRoleAssigned(candidate, APOLLO)).to.be.false;
   });
 
   it('assigns and unassigns a role to the address', async () => {
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    expect(await getRoleAssigned(other)).to.equal('3');
-    expect(await hasRoleAssigned(other, APOLLO)).to.be.true;
-    expect(await hasRoleAssigned(other, HERMES)).to.be.false;
-    await removeFromWhitelist(other);
-    expect(await hasRoleAssigned(other, APOLLO)).to.be.false;
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    expect(await getRoleAssigned(candidate)).to.equal('3');
+    expect(await hasRoleAssigned(candidate, APOLLO)).to.be.true;
+    expect(await hasRoleAssigned(candidate, HERMES)).to.be.false;
+    await removeFromWhitelist(candidate);
+    expect(await hasRoleAssigned(candidate, APOLLO)).to.be.false;
   });
 
   it('correctly stores required deposit', async () => {
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    expect(await getRequiredDeposit(other)).to.equal(APOLLO_DEPOSIT.toString());
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    expect(await getRequiredDeposit(candidate)).to.equal(APOLLO_DEPOSIT.toString());
   });
 
   it('is possible to assign role after it was unassigned', async () => {
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    await removeFromWhitelist(other);
-    await addToWhitelist(other, HERMES, 0);
-    expect(await hasRoleAssigned(other, APOLLO)).to.be.false;
-    expect(await hasRoleAssigned(other, HERMES)).to.be.true;
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    await removeFromWhitelist(candidate);
+    await addToWhitelist(candidate, HERMES, 0);
+    expect(await hasRoleAssigned(candidate, APOLLO)).to.be.false;
+    expect(await hasRoleAssigned(candidate, HERMES)).to.be.true;
   });
 
   it(`throws if non owner attempts to add`, async () => {
-    await expect(addToWhitelist(other, totalStranger, APOLLO_DEPOSIT)).to.be.eventually.rejected;
-    expect(await isWhitelisted(other)).to.equal(false);
+    await expect(addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT, notOwner)).to.be.eventually.rejected;
+    expect(await isWhitelisted(candidate)).to.equal(false);
   });
 
   it(`throws if non owner attempts to remove`, async () => {
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    await expect(removeFromWhitelist(other, totalStranger)).to.be.eventually.rejected;
-    expect(await isWhitelisted(other)).to.be.true;
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    await expect(removeFromWhitelist(candidate, notOwner)).to.be.eventually.rejected;
+    expect(await isWhitelisted(candidate)).to.be.true;
   });
 
   it('throws on try to assign nonexisting role', async () => {
-    await expect(addToWhitelist(other, 0, APOLLO_DEPOSIT)).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, 420, APOLLO_DEPOSIT)).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, 0, APOLLO_DEPOSIT)).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, 420, APOLLO_DEPOSIT)).to.be.eventually.rejected;
   });
 
   it('throws if trying to whitelist Apollo with 0 deposit', async () => {
-    await expect(addToWhitelist(other, APOLLO, 0)).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, APOLLO, 0)).to.be.eventually.rejected;
   });
 
   it('throws if trying to whitelist Atlas with a stake not included in config', async () => {
-    await expect(addToWhitelist(other, ATLAS, ATLAS1_STAKE.sub(ONE))).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, ATLAS, ATLAS1_STAKE.add(ONE))).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, ATLAS, ATLAS2_STAKE.sub(ONE))).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, ATLAS, ATLAS2_STAKE.add(ONE))).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, ATLAS, ATLAS3_STAKE.sub(ONE))).to.be.eventually.rejected;
-    await expect(addToWhitelist(other, ATLAS, ATLAS3_STAKE.add(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS1_STAKE.sub(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS1_STAKE.add(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS2_STAKE.sub(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS2_STAKE.add(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS3_STAKE.sub(ONE))).to.be.eventually.rejected;
+    await expect(addToWhitelist(candidate, ATLAS, ATLAS3_STAKE.add(ONE))).to.be.eventually.rejected;
   });
 
-  it('throws if whitelisting the address that is already whitelisted', async () => {
-    await addToWhitelist(other, APOLLO, APOLLO_DEPOSIT);
-    await expect(addToWhitelist(other, HERMES, 0)).to.be.eventually.rejected;
+  it('throws if whitelisting a address that is already whitelisted', async () => {
+    await addToWhitelist(candidate, APOLLO, APOLLO_DEPOSIT);
+    await expect(addToWhitelist(candidate, HERMES, 0)).to.be.eventually.rejected;
   });
 });
