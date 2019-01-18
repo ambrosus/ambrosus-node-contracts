347919077230350af25fd1534f8b8e2e24d9a243
Antoni Kedracki <antoni@ethworks.io>
2019-01-18 10:26:24 +0100

    Restrict whitelisting deposit amount to be at least 250k AMB;

--- contracts/Front/KycWhitelist.sol
+++ contracts/Front/KycWhitelist.sol
@@ -57,7 +57,7 @@ contract KycWhitelist is Ownable {
 
     function requireCorrectDeposit(Consts.NodeType role, uint deposit) private view {
         if (role == Consts.NodeType.APOLLO) {
-            require(deposit > 0);
+            require(deposit >= config.APOLLO_DEPOSIT());
         } else if (role == Consts.NodeType.ATLAS) {
             require(deposit == config.ATLAS1_STAKE() || deposit == config.ATLAS2_STAKE() || deposit == config.ATLAS3_STAKE());
         }
