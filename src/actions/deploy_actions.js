c6e30251b7d2145970bafe677e189e52332f9bc2
Andrii Rozinko <a.rozinko@minerall.io>
2019-07-20 21:37:30 +0300

    Temporary patch of update contracts procedure for redeploy ShelteringTransfersStore contract.

--- src/actions/deploy_actions.js
+++ src/actions/deploy_actions.js
@@ -58,7 +58,11 @@ export default class DeployActions {
   async recycleStorageContracts() {
     const storageContractNames = this.headWrapper.availableStorageCatalogueContracts;
     const recycled = {};
+    const doPatch = this.contractsVersion() === '0.0.58';
     for (const contractName of storageContractNames) {
+      if (doPatch && contractName === 'shelteringTransfersStore') {
+        continue;
+      }
       recycled[contractName] = await this.headWrapper.contractAddressByName(contractName);
     }
     return recycled;
