274d686f0a5ab5ab7d2ab29a6bcb3d316e77b24c
Sergiy Chayka <schayka@gmail.com>
2020-02-05 01:38:53 -0500

    atlas withdrawl event add

--- contracts/Front/Payouts.sol
+++ contracts/Front/Payouts.sol
@@ -38,6 +38,8 @@ contract Payouts is Base {
     function withdraw(address target) public {
         uint64 payoutPeriod = time.currentPayoutPeriod().sub(1).castTo64();
         require(address(payoutsStore).call(bytes4(keccak256("withdraw(address,address,uint64)")), msg.sender, target, payoutPeriod));
+
+        rewardsEventEmitter.atlasPayoutWithdrawal(target, msg.sender, 0);
     }
 
     function available(uint64 payoutPeriod) public view returns (uint) {
