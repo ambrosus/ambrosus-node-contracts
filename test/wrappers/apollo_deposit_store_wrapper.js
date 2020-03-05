dddc47c47e67cd880b9a97adb375f17757441908
Andrii Rozinko <a.rozinko@minerall.io>
2020-03-05 11:28:35 +0200

    Add apolloDepositStoreWrapper

---
+++ test/wrappers/apollo_deposit_store_wrapper.js
@@ -0,0 +1,48 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import ApolloDepositStoreWrapper from '../../src/wrappers/apollo_deposit_store_wrapper';
+
+chai.use(sinonChai);
+const {expect} = chai;
+
+describe('Apollo deposit store wrapper', () => {
+  let apolloDepositStoreWrapper;
+
+  let isDepositingStub;
+  let isDepositingCallStub;
+
+  const nodeAddress = '0xc0ffee';
+
+  beforeEach(async () => {
+    isDepositingStub = sinon.stub();
+    isDepositingCallStub = sinon.stub();
+
+    const contractMock = {
+      methods: {
+        isDepositing: isDepositingStub.returns({
+          call: isDepositingCallStub.resolves(true)
+        })
+      }
+    };
+    apolloDepositStoreWrapper = new ApolloDepositStoreWrapper();
+    sinon.stub(apolloDepositStoreWrapper, 'contract').resolves(contractMock);
+  });
+
+  describe('isDepositing', () => {
+    it('calls contract method with correct arguments', async () => {
+      expect(await apolloDepositStoreWrapper.isDepositing(nodeAddress)).to.equal(true);
+      expect(isDepositingStub).to.be.calledOnceWith(nodeAddress);
+      expect(isDepositingCallStub).to.be.calledOnce;
+    });
+  });
+});
