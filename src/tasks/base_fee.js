f7e6687e15153187fee8689a119448966222b13b
Andrii Rozinko <a.rozinko@minerall.io>
2021-09-13 09:38:50 +0300

    Add fees settings store/restore

--- src/tasks/base_fee.js
+++ src/tasks/base_fee.js
@@ -20,6 +20,24 @@ export default class BaseFeeTask extends TaskBase {
       await this.getBaseFee();
     } else if (command === 'set') {
       await this.setBaseFee(options[0]);
+    } else if (command === 'getDeveloper') {
+      await this.getDeveloper();
+    } else if (command === 'setDeveloper') {
+      await this.setDeveloper(options[0]);
+    } else if (command === 'getDeveloperFee') {
+      await this.getDeveloperFee();
+    } else if (command === 'setDeveloperFee') {
+      await this.setDeveloperFee(options[0]);
+    } else if (command === 'getDeveloperUploadFee') {
+      await this.getDeveloperUploadFee();
+    } else if (command === 'setDeveloperUploadFee') {
+      await this.setDeveloperUploadFee(options[0]);
+    } else if (command === 'getAdmins') {
+      await this.getAdmins();
+    } else if (command === 'setAdmins') {
+      for (const admin of options) {
+        await this.addAdmin(admin);
+      }
     } else {
       console.error('Unknown sub-command, see yarn task fee help');
       process.exit(1);
@@ -32,6 +50,38 @@ export default class BaseFeeTask extends TaskBase {
 
   async setBaseFee(newBaseFee) {
     return this.feesWrapper.setBaseUploadFee(newBaseFee);
+  }
+
+  async getDeveloper() {
+    return this.feesWrapper.getDeveloper();
+  }
+
+  async setDeveloper(developer) {
+    return this.feesWrapper.setDeveloper(developer);
+  }
+
+  async getDeveloperFee() {
+    return this.feesWrapper.getDeveloperFee();
+  }
+
+  async setDeveloperFee(fee) {
+    return this.feesWrapper.setDeveloperFee(fee);
+  }
+
+  async getDeveloperUploadFee() {
+    return this.feesWrapper.getDeveloperUploadFee();
+  }
+
+  async setDeveloperUploadFee(fee) {
+    return this.feesWrapper.setDeveloperUploadFee(fee);
+  }
+
+  async getAdmins() {
+    console.log((await this.feesWrapper.getAdmins()).join(','));
+  }
+
+  async addAdmin(admin) {
+    return this.feesWrapper.addAdmin(admin);
   }
 
   help() {
