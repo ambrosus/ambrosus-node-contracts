c897bbd12f7240dac05d8dd307de0b3c879be7ac
kiw <anon@anon.com>
2022-03-17 13:56:58 +0200

    add modifiedValidatorSet

---
+++ contracts/Consensus/modifiedValidatorSet.sol
@@ -0,0 +1,99 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.io
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+pragma solidity ^0.4.22;
+
+import "./ConstructorOwnable.sol";
+import "./ValidatorSet.sol";
+
+
+/**
+@title Implementation of Parities ValidatorSet contract with:
+- simple add/remove methods
+- only owner (set explicitly in constructor and transferable) can perform mutating functions
+*/
+contract ModifiedValidatorSet is ValidatorSetBase, ConstructorOwnable {
+    address private superUser; // the SUPER_USER address as defined by EIP96. During normal operation should be 2**160 - 2
+    address[] public validators;
+    address[] public pendingValidators;
+
+    /**
+    @notice Constructor
+    @param _owner the owner of this contract, that can add/remove validators.
+    @param _initialValidators the initial set of validator addresses.
+    @param _superUser SUPER_USER address value injectable for test purpouses. Under normal operation it should be set to 2^160-2 as defined in EIP96
+    */
+    constructor(address _owner, address[] _initialValidators, address _superUser) public ConstructorOwnable(_owner) {
+        require(_initialValidators.length > 0);
+        require(_superUser != address(0));
+        superUser = _superUser;
+        validators = _initialValidators;
+        pendingValidators = _initialValidators;
+    }
+
+    modifier inArray(address _subject, address[] _array, string _message) {
+        require(checkInArray(_subject, _array), _message);
+        _;
+    }
+
+    modifier notInArray(address _subject, address[] _array, string _message) {
+        require(!checkInArray(_subject, _array), _message);
+        _;
+    }
+
+    function getValidators() public view returns (address[]) {
+        return validators;
+    }
+
+    function getPendingValidators() public view returns (address[]) {
+        return pendingValidators;
+    }
+
+    function addValidator(address _validator) public onlyOwner notInArray(_validator, pendingValidators, "Provided address is already a validator") {
+        // Should shuffle Validator Set for security. So one person / collusive group
+        // can not assemble a chain of their nodes in succession at the end of the set
+        uint index = block.number % pendingValidators.length;
+        pendingValidators.push(pendingValidators[index]);
+        pendingValidators[index] = _validator;
+
+        emitChangeEvent();
+    }
+
+    function removeValidator(address _validator) public onlyOwner inArray(_validator, pendingValidators, "Provided address is not a validator") {
+        for (uint i = 0; i < pendingValidators.length; ++i) {
+            if (pendingValidators[i] == _validator) {
+                pendingValidators[i] = pendingValidators[pendingValidators.length - 1];
+                delete pendingValidators[pendingValidators.length - 1];
+                pendingValidators.length--;
+            }
+        }
+
+        emitChangeEvent();
+    }
+
+    function finalizeChange() public {
+        require(msg.sender == superUser, "Must be called by super user");
+
+        validators = pendingValidators;
+    }
+
+    function emitChangeEvent() private {
+        /* solium-disable-next-line security/no-block-members */
+        emit InitiateChange(blockhash(block.number - 1), pendingValidators);
+    }
+
+    function checkInArray(address _subject, address[] _array) private pure returns(bool) {
+        for (uint i = 0; i < _array.length; ++i) {
+            if (_array[i] == _subject) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ test/contracts/consensus/modified_validator_set.js
@@ -0,0 +1,236 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.io
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import {createWeb3, deployContract, makeSnapshot, restoreSnapshot} from '../../../src/utils/web3_tools';
+import chaiEmitEvents from '../../helpers/chaiEmitEvents';
+import ModifiedValidatorSetJson from '../../../src/contracts/ModifiedValidatorSet.json';
+
+const {expect} = chai;
+chai.use(chaiAsPromised);
+chai.use(chaiEmitEvents);
+
+describe('Validator set contract', () => {
+  const standardOptions = {
+    gas: 1000000
+  };
+
+  const deploy = async (web3, sender, owner, initialValidators, superUser) => deployContract(web3, ModifiedValidatorSetJson, [owner, initialValidators, superUser], {from: sender});
+  const getOwner = async (contract) => contract.methods.owner().call();
+  const transferOwnership = async (contract, sender, newOwner) => contract.methods.transferOwnership(newOwner).send({...standardOptions, from: sender});
+  const addValidator = async (contract, sender, validator) => contract.methods.addValidator(validator).send({...standardOptions, from: sender});
+  const removeValidator = async (contract, sender, validator) => contract.methods.removeValidator(validator).send({...standardOptions, from: sender});
+  const getValidators = async (contract) => contract.methods.getValidators().call();
+  const getPendingValidators = async (contract) => contract.methods.getPendingValidators().call();
+  const finalizeChange = async (contract, sender) => contract.methods.finalizeChange().send({...standardOptions, from: sender});
+
+  let web3;
+  let deployer;
+  let owner;
+  let otherUser;
+  let superUser;
+  let newOwner;
+  let exampleValidatorAddresses;
+  let additionalValidator;
+
+  before(async () => {
+    web3 = await createWeb3();
+    [deployer, owner, otherUser, superUser, newOwner] = await web3.eth.getAccounts();
+    exampleValidatorAddresses = Array(3)
+      .fill(null)
+      .map(() => web3.eth.accounts.create().address);
+    additionalValidator = web3.eth.accounts.create().address;
+  });
+
+  describe('constructor', () => {
+    let contract;
+
+    before(async () => {
+      contract = await deploy(web3, deployer, owner, exampleValidatorAddresses, superUser);
+    });
+
+    it('sets the initial validators from the parameter', async () => {
+      const storedValidators = await getValidators(contract);
+      expect(storedValidators).to.have.same.members(exampleValidatorAddresses);
+    });
+
+    it('sets the owner', async () => {
+      const storedOwner = await getOwner(contract);
+      expect(storedOwner).to.equal(owner);
+
+      // expect(false).to.be.true;
+    });
+  });
+
+  describe('constructor - fail safes', () => {
+    // NOTE: web3 incorrectly reports a failed deployment as successful, as a workaround we try to call a different method which should then fail.
+    const zeroAddress = '0x0000000000000000000000000000000000000000';
+    it('throws if no initial validators are provided', async () => {
+      await expect(deploy(web3, deployer, owner, [], superUser)).to.eventually.be.rejected;
+    });
+
+    it('throws if super user  is 0x0', async () => {
+      await expect(deploy(web3, deployer, owner, exampleValidatorAddresses, zeroAddress)).to.eventually.be.rejected;
+    });
+
+    it('throws if owner is 0x0', async () => {
+      await expect(deploy(web3, deployer, zeroAddress, exampleValidatorAddresses, superUser)).to.eventually.be.rejected;
+    });
+  });
+
+  describe('adding a validator', () => {
+    let contract;
+    let snapshotId;
+
+    before(async () => {
+      contract = await deploy(web3, deployer, owner, exampleValidatorAddresses, superUser);
+    });
+
+    beforeEach(async () => {
+      snapshotId = await makeSnapshot(web3);
+    });
+
+    afterEach(async () => {
+      await restoreSnapshot(web3, snapshotId);
+    });
+
+    it('adds the new validator to the pendingValidators list', async () => {
+      await expect(addValidator(contract, owner, additionalValidator)).to.be.fulfilled;
+      expect(await getPendingValidators(contract)).to.have.same.members([...exampleValidatorAddresses, additionalValidator]);
+    });
+
+    it(`doesn't change the validator list`, async () => {
+      await expect(addValidator(contract, owner, additionalValidator)).to.be.fulfilled;
+      expect(await getValidators(contract)).to.have.same.members(exampleValidatorAddresses);
+    });
+
+    it(`fails on duplicate insertions`, async () => {
+      expect(await getPendingValidators(contract)).to.have.same.members(exampleValidatorAddresses);
+      await expect(addValidator(contract, owner, additionalValidator)).to.be.fulfilled;
+      expect(await getPendingValidators(contract)).to.have.same.members([...exampleValidatorAddresses, additionalValidator]);
+      await expect(addValidator(contract, owner, additionalValidator)).to.be.rejected;
+      expect(await getPendingValidators(contract)).to.have.same.members([...exampleValidatorAddresses, additionalValidator]);
+    });
+
+    it('emits a InitiateChange event', async () => {
+      expect(await addValidator(contract, owner, additionalValidator)).to.emitEvent('InitiateChange');
+    });
+
+    it(`can't be invoked by non-owner`, async () => {
+      await expect(addValidator(contract, otherUser, additionalValidator)).to.be.rejected;
+    });
+  });
+
+  describe('removing a validator', () => {
+    let contract;
+    let initialValidator;
+    let initialValidators;
+    let nonValidator;
+    let snapshotId;
+
+    before(async () => {
+      [initialValidator, nonValidator] = exampleValidatorAddresses;
+      initialValidators = [initialValidator];
+      contract = await deploy(web3, deployer, owner, initialValidators, superUser);
+    });
+
+    beforeEach(async () => {
+      snapshotId = await makeSnapshot(web3);
+    });
+
+    afterEach(async () => {
+      await restoreSnapshot(web3, snapshotId);
+    });
+
+    it('removes the validator from the pendingValidators list', async () => {
+      await expect(removeValidator(contract, owner, initialValidator)).to.be.fulfilled;
+      expect(await getPendingValidators(contract)).to.be.empty;
+    });
+
+    it(`doesn't change the validator list`, async () => {
+      await expect(removeValidator(contract, owner, initialValidator)).to.be.fulfilled;
+      expect(await getValidators(contract)).to.have.same.members(initialValidators);
+    });
+
+    it(`fails when removing an validator that never was registered`, async () => {
+      await expect(removeValidator(contract, owner, nonValidator)).to.be.rejected;
+    });
+
+    it(`fails when removing an previously registered validator twice`, async () => {
+      await expect(removeValidator(contract, owner, initialValidator)).to.be.fulfilled;
+      await expect(removeValidator(contract, owner, initialValidator)).to.be.rejected;
+    });
+
+    it('emits a InitiateChange event', async () => {
+      expect(await removeValidator(contract, owner, initialValidator)).to.emitEvent('InitiateChange');
+    });
+
+    it(`can't be invoked by non-owner`, async () => {
+      await expect(removeValidator(contract, otherUser, initialValidators)).to.be.rejected;
+    });
+  });
+
+  describe('finalising the validators change', () => {
+    let contract;
+
+    before(async () => {
+      contract = await deploy(web3, deployer, owner, exampleValidatorAddresses, superUser);
+    });
+
+    // Note: according to spec the finalizeChange method is called internally by Parity using the special SUPER_USER address.
+    // As we can't use it our selfs for tests, we inject a different address using the constructor
+    it('copies the contents of the pendingValidators array into the validators array', async () => {
+      await addValidator(contract, owner, additionalValidator);
+      expect(await getPendingValidators(contract)).to.not.have.same.members(await getValidators(contract));
+
+      await expect(finalizeChange(contract, superUser)).to.be.fulfilled;
+
+      expect(await getPendingValidators(contract)).to.have.same.members(await getValidators(contract));
+    });
+
+    it('fails for non-SUPER_USER', async () => {
+      await expect(finalizeChange(contract, owner)).to.be.rejected;
+    });
+  });
+
+  describe('transferring ownership', () => {
+    let contract;
+    let snapshotId;
+
+    before(async () => {
+      contract = await deploy(web3, deployer, owner, exampleValidatorAddresses, superUser);
+    });
+
+    beforeEach(async () => {
+      snapshotId = await makeSnapshot(web3);
+    });
+
+    afterEach(async () => {
+      await restoreSnapshot(web3, snapshotId);
+    });
+
+    it('sets the new owner', async () => {
+      await expect(transferOwnership(contract, owner, newOwner)).to.be.fulfilled;
+      expect(await getOwner(contract)).to.equal(newOwner);
+    });
+
+    it('emits a OwnershipTransferred event', async () => {
+      expect(await transferOwnership(contract, owner, newOwner)).to.emitEvent('OwnershipTransferred');
+    });
+
+    it(`to 0x0 address is not allowed`, async () => {
+      await expect(transferOwnership(contract, owner, '0x0')).to.be.rejected;
+    });
+
+    it(`can't be invoked by non-owner`, async () => {
+      await expect(transferOwnership(contract, otherUser, newOwner)).to.be.rejected;
+    });
+  });
+});
